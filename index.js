79a1553cfa0a92da8f785c8c8f9db6be5659f35c
Himanshu Bhargava <himanshuiitd95@gmail.com>
2024-06-13 23:26:57 +0530

    ACtions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+  };
+  
